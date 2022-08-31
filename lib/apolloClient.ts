b035679656410762f6ea9572c018a885b606c752
takumi_kurogi <takumines1@gmail.com>
2022-09-01 00:48:13 +0900

    GraphQLからデータを取得する処理の実装

---
+++ lib/apolloClient.ts
@@ -0,0 +1,26 @@
+import { ApolloClient, HttpLink, InMemoryCache, NormalizedCacheObject } from '@apollo/client'
+import 'cross-fetch/polyfill'
+
+export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__'
+
+let apolloClient: ApolloClient<NormalizedCacheObject> | undefined
+
+const createApolloClient = () => {
+  return new ApolloClient({
+    ssrMode: typeof window === 'undefined',
+    link: new HttpLink({
+      uri: process.env.NEXT_PUBLIC_HASURA_URI,
+    }),
+    cache: new InMemoryCache(),
+  })
+}
+
+export const initializeApollo = (initialState = null) => {
+  const _apolloClient = apolloClient ?? createApolloClient()
+  // For SSG and SSR always create a new Apollo Client
+  if (typeof window === 'undefined') return _apolloClient
+  // Create the Apollo Client once in the Client
+  if (!apolloClient) apolloClient = _apolloClient
+
+  return _apolloClient
+}
